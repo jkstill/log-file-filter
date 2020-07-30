1bbb011cc7f0f793bab0b4a4da691cf552a8d329
Jared Still <jkstill@gmail.com>
2020-07-30 11:43:01 -0700

    added grep colors to the config line

--- log-filter.sh
+++ log-filter.sh
@@ -8,6 +8,12 @@ declare -r msgFile=$2
 # throw an error if an undefined variable is referenced
 set -u
 
+# default GREP_COLORS - black on white
+# 256 color
+#export GREP_COLORS='mt=38;5;16;48;5;231'
+# 8 color
+export GREP_COLORS='mt=30;47'
+
 [[ -z $rulesFile ]] && { echo $0 rules-file log-file; exit 1; }
 [[ -r $rulesFile ]] || { echo cannot read $rulesFile; exit 2; }
 
@@ -17,13 +23,14 @@ set -u
 declare configLine
 configLine=$(tail -n+2 $rulesFile  | head -1)
 
-declare ruleType contextLinesBefore contextLinesAfter caseSensitive grepColor
+declare ruleType contextLinesBefore contextLinesAfter caseSensitive grepColor grepColors
 
 ruleType=$(echo $configLine | cut -f1 -d:)
 contextLinesBefore=$(echo $configLine | cut -f2 -d:)
 contextLinesAfter=$(echo $configLine | cut -f3 -d:)
 caseSensitive=$(echo $configLine | cut -f4 -d:)
 grepColor=$(echo $configLine | cut -f5 -d:)
+grepColors=$(echo $configLine | cut -f6 -d:)
 
 declare grepIncExFlag
 
@@ -37,6 +44,10 @@ else
 	echo
 	exit 1
 fi
+
+# validate the color codes
+echo $grepColors | grep -E 'mt=3[0-9];4[0-9]|mt=38;5;[0-9]{1,3};48;5;[0-9]{1,3}' > /dev/null
+[[ (( $? == 0 )) ]] && { export GREP_COLORS=$grepColors; }
 
 declare contextBeforeOption='' contextAfterOption=''
 
@@ -53,18 +64,6 @@ case $grepColor in
 	A|a) grepColorFlag='auto';;
 	N|n) grepColorFlag='never';;
 esac
-
-# set GREP_COLORS to highlight the matched part of a line
-# white on blue
-#export GREP_COLORS='mt=38;5;223;48;5;19'
-# yellow on black
-#export GREP_COLORS='mt=38;5;16;48;5;227'
-
-# black on white
-# 256 color
-export GREP_COLORS='mt=38;5;16;48;5;231'
-# 8 color
-#export GREP_COLORS='mt=30;47'
 
 : << 'GREP-COLOR-COMMENTS'
 
